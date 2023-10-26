644dcc80572e6d30cf88126a6cf41229c37c0cb2
jithsg <jithishjsg@gmail.com>
2023-10-26 17:17:08 +0800

    change eval.py

--- src/eval.py
+++ src/eval.py
@@ -3,14 +3,19 @@ from typing import Text
 import pandas as pd
 import joblib
 import json
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, f1_score
+from sklearn.metrics import (
+    confusion_matrix, ConfusionMatrixDisplay, f1_score,
+    precision_score, recall_score, accuracy_score
+)
 import matplotlib.pyplot as plt
 import argparse
-def eval(config_path:Text)->None:
+
+def eval(config_path: Text) -> None:
     with open(config_path) as conf_file:
-        config= yaml.safe_load(conf_file)
+        config = yaml.safe_load(conf_file)
         model = joblib.load(config['training']['model_path'])
     test_dataset = pd.read_csv(config['data']['testset_path'])
+
     # Prepare test data
     y_test = test_dataset.loc[:, 'target'].values.astype('int32')
     X_test = test_dataset.drop('target', axis=1).values.astype('float32')
@@ -21,6 +26,9 @@ def eval(config_path:Text)->None:
     # Generate confusion matrix
     cm = confusion_matrix(y_test, prediction)
     f1 = f1_score(y_true=y_test, y_pred=prediction, average='macro')
+    precision = precision_score(y_true=y_test, y_pred=prediction, average='macro')
+    recall = recall_score(y_true=y_test, y_pred=prediction, average='macro')
+    accuracy = accuracy_score(y_true=y_test, y_pred=prediction)
 
     # Create a Confusion Matrix Display
     disp = ConfusionMatrixDisplay(confusion_matrix=cm)
@@ -29,26 +37,22 @@ def eval(config_path:Text)->None:
     # Save the figure
     plt.savefig(config['reports']['confusion_matrix_file'])
 
-    # Show the plot
-
-    metrics_file = config['reports']['metrics_file']
-
+    # Metrics
     metrics = {
-        'f1': f1
+        'f1': f1,
+        'precision': precision,
+        'recall': recall,
+        'accuracy': accuracy
     }
 
-    with open(metrics_file, 'w') as mf:
-        json.dump(
-            obj=metrics,
-            fp=mf,
-            indent=4
-        )
+    with open(config['reports']['metrics_file'], 'w') as mf:
+        json.dump(obj=metrics, fp=mf, indent=4)
+
     print('Evaluation complete.')
-    
+
 if __name__ == '__main__':
-    argparser= argparse.ArgumentParser()
+    argparser = argparse.ArgumentParser()
     argparser.add_argument("--config", dest="config", required=True)
     args = argparser.parse_args()
-       
-    
+
     eval(config_path=args.config)
